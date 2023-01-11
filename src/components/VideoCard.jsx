60677faab9a7edece9ac645a686e71a5c793d14c
Franco <ougaoyuan0718@gmail.com>
2023-01-12 00:58:04 +1100

    showing video thumbnails on homepage

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {Typography, Card, CardContent, CardMedia} from "@mui/material";
+import {CheckCircle} from "@mui/icons-material";
+import {demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle} from "../utils/constants";
 
-function VideoCard(props) {
+// shows the basic information of video including title,
+function VideoCard({video:{id:{videoId},snippet}}) {
+    console.log(videoId,snippet)
     return (
-        <div>Video Card</div>
+        <Card>
+            {/*question marks to avoid undefined*/}
+            {/* navigate to customized video page*/}
+            {/*<Link to={`https://www.youtube.com/${videoId}`}>*/}
+
+            {/* navigate to original YouTube video page*/}
+            <a href={`https://www.youtube.com/watch?v=${videoId}`}>
+                <CardMedia
+                    sx={{width: 358, height: 180}}
+                    alt={snippet?.title}
+                    image={snippet?.thumbnails?.high?.url}/>
+            </a>
+                {/*</Link>*/}
+        </Card>
     );
 }
 
